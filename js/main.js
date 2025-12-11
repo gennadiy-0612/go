9861e6e3804e42c72fb3b2940512a645aa2ef7c3
Hennadiy <fotoxsppua@gmail.com>
2025-12-11 12:46:22 +0200

    Remove class addition for inactive slides in showSlides function

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].classList.add("noActSlide");  
+    slides[i].className.replace("noActSlide",""); 
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
